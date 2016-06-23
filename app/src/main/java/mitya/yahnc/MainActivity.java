376c6b3f3ad1ed281cca88b789293e1c4371eebf
Mitya <dmitriy.kobzarev@gmail.com>
2016-06-23 11:05:05 +0300

    Added toolbar and refresh icon

--- app/src/main/java/mitya/yahnc/MainActivity.java
+++ app/src/main/java/mitya/yahnc/MainActivity.java
@@ -2,6 +2,10 @@ package mitya.yahnc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.mainToolbar);
+        setSupportActionBar(mainToolbar);
+        getSupportActionBar().setTitle(this.getClass().getSimpleName());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                //code for refreshing the page
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
