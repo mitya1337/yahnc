1943eb984fddf405f0db51df6dc47c8a90cee66b
Mitya <dmitriy.kobzarev@gmail.com>
2016-06-26 21:58:00 +0300

    Add subscriber.onError method call into observable

--- app/src/main/java/mitya/yahnc/MainActivity.java
+++ app/src/main/java/mitya/yahnc/MainActivity.java
@@ -57,7 +57,9 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     story = parser.parse(getResources().openRawResource(R.raw.json));
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    if (!subscriber.isUnsubscribed()){
+                        subscriber.onError(e);
+                    }
                 }
                 for (int i = 0; i < 20; i++) {
                     stories.add(story);
