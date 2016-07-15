f666a938eebb26ed21dc669867cbe8e2a79f5580
Mitya <dmitriy.kobzarev@gmail.com>
2016-07-16 02:15:10 +0300

    Add string resources for 'Today' and 'Yestarday' words

--- app/src/main/java/mitya/yahnc/FormatUtils.java
+++ app/src/main/java/mitya/yahnc/FormatUtils.java
@@ -20,9 +20,9 @@ public class FormatUtils {
         DateFormat timeFormatter = new SimpleDateFormat("hh:mma");
 
         if (calendar.get(Calendar.YEAR) == today.get(Calendar.YEAR) && calendar.get(Calendar.DAY_OF_YEAR) == today.get(Calendar.DAY_OF_YEAR)) {
-            return "Today " + timeFormatter.format(dateTime);
+            return R.string.today + timeFormatter.format(dateTime);
         } else if (calendar.get(Calendar.YEAR) == yesterday.get(Calendar.YEAR) && calendar.get(Calendar.DAY_OF_YEAR) == yesterday.get(Calendar.DAY_OF_YEAR)) {
-            return "Yesterday " + timeFormatter.format(dateTime);
+            return R.string.yesterday + timeFormatter.format(dateTime);
         } else {
             return dateTime.toString();
         }
