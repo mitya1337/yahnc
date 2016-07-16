333d19d4c0aec37e685859961e83a7224ed57cb8
Mitya <dmitriy.kobzarev@gmail.com>
2016-07-16 06:46:06 +0300

    Add margin between items in story list

--- app/src/main/java/mitya/yahnc/MainActivity.java
+++ app/src/main/java/mitya/yahnc/MainActivity.java
@@ -1,6 +1,5 @@
 package mitya.yahnc;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -12,7 +11,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
